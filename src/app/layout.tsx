284f41e277c14c02a0b84ae279c13ff6bc5befac
Mohit Gadhavi <mohit.gadhavi@zybisys.com>
2023-03-09 18:00:36 +0530

    basic layout implemented

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,18 @@
-import './globals.css'
+import "./globals.css";
 
 export const metadata = {
-  title: 'Playground',
-  description: 'comming soon',
-}
+	title: "Playground",
+	description: "comming soon",
+};
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode
+	children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  )
+	return (
+		<html lang="en">
+			<body>{children}</body>
+		</html>
+	);
 }
