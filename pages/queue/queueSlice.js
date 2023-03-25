36295d32b007f3375bb10c804a88e59320e87334
md shahbaz <mdshahbazwarish1996@gmail.com>
2023-03-25 12:23:51 +0530

    added linked list

--- pages/queue/queueSlice.js
+++ pages/queue/queueSlice.js
@@ -4,21 +4,21 @@ const initialState = {
   data: [],
 };
 
-export const stackSlice = createSlice({
-  name: "stack",
+export const queueSlice = createSlice({
+  name: "queue",
   initialState,
   reducers: {
-    StackArr: (state, actions) => {
-      //console.log("actions", actions.payload);
+    QueueArr: (state, actions) => {
+      console.log("actions", actions.payload);
       const { name, type } = actions.payload;
       if (type.includes("push")) {
         state.data = [name, ...state.data];
       }
       if (type.includes("pop")) {
-        state.data = state.data.slice(1, state.data.length);
+        state.data.pop();
       }
     },
   },
 });
-export const { StackArr } = stackSlice.actions;
-export default stackSlice.reducer;
+export const { QueueArr } = queueSlice.actions;
+export default queueSlice.reducer;
